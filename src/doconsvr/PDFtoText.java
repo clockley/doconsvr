0636a5c18e992849dfb4348eb2c3892b01932fed
Christian <clockley1@gmail.com>
2019-09-10 10:41:09 -0400

    simpler solution

--- src/doconsvr/PDFtoText.java
+++ src/doconsvr/PDFtoText.java
@@ -18,34 +18,13 @@ class PDFtoText {
             pdfParser.parse();
             PDDocument pdDocument = new PDDocument(pdfParser.getDocument());
             PDFTextStripper pdfTextStripper = new PDFLayoutTextStripper();
-            string = pdfTextStripper.getText(pdDocument);
-            //https://stackoverflow.com/questions/6198986/how-can-i-replace-non-printable-unicode-characters-in-java
-            cleanString = new StringBuilder(string.length());
-            for (int offset = 0; offset < cleanString.length();) {
-                int codePoint = string.codePointAt(offset);
-                offset += Character.charCount(codePoint);
-
-                // Replace invisible control characters and unused code points
-                switch (Character.getType(codePoint))
-                {
-                    case Character.CONTROL:     // \p{Cc}
-                    case Character.FORMAT:      // \p{Cf}
-                    case Character.PRIVATE_USE: // \p{Co}
-                    case Character.SURROGATE:   // \p{Cs}
-                    case Character.UNASSIGNED:  // \p{Cn}
-                    	cleanString.append('?');
-                        break;
-                    default:
-                    	cleanString.append(Character.toChars(codePoint));
-                        break;
-                }  	
-            }
+            string = pdfTextStripper.getText(pdDocument).replaceAll("\\p{C}", "");
             pdDocument.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         };
-        return cleanString.toString();
+        return string;
 	}
 }
